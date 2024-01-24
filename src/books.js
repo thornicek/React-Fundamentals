db45746c78ffb193dc1c1b0d84d04838476fcfaa
thornicek <tomas.hornicek8@email.cz>
2024-01-24 18:27:20 -0500

    React Fundamentals Finished

---
+++ src/books.js
@@ -0,0 +1,25 @@
+import img1 from './images/AtomicHabits.jpg'
+import img2 from './images/FourthWing.jpg'
+import img3 from './images/Power.jpg'
+
+export const books = [
+  {
+    author: 'James Clear',
+    title:
+      'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
+    img: img1,
+    id: 1,
+  },
+  {
+    author: 'Rebecca Yarros',
+    title: 'Fourth Wing (The Empyrean, 1)',
+    img: img2,
+    id: 2,
+  },
+  {
+    author: 'Robert Greene',
+    title: 'The 48 Laws of Power',
+    img: img3,
+    id: 3,
+  }
+];
--- src/index.js
+++ src/index.js
@@ -2,48 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-
-const books = [
-  {
-  author: 'James Clear',
-  title:
-    'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
-  img: './images/AtomicHabits.jpg',
-  id: 1
-  },
-  {
-    author: 'Rebecca Yarros',
-    title: 'Fourth Wing (The Empyrean, 1)',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/91n7p-j5aqL._AC_UL600_SR600,400_.jpg',
-    id: 2
-  }
-]
+import {books} from './books'
+import Book from './Book'
 
 
-const BookList = () => {
+
+function BookList() {
   return (
+    <>
+    <h1>Amazon Best Sellers</h1>
     <section className="booklist">
-      {books.map((book)=>{
-          const { img, title, author, id } = book
+      {books.map((book, index) => {
+        const { img, title, author, id } = book;
         return (
-         <Book {...book} key={id} />
-        )
+        
+          <Book {...book} key={book.id} number={index}/>
+        );
       })}
     </section>
-  )
-};
+    </>
+  );
+}
 
-const Book = (props) => {
-  const { img, title, author } = props; 
-  return <article className='book'>
-      <img
-    src={img}
-    alt={title}
-    />
-    <h2>{title}</h2>
-    <h4>{author}</h4>
-  </article>;
-};
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
